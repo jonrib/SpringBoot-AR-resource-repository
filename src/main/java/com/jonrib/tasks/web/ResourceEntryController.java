00bfefda5e18c00be14ed81b935bfe3e2e6abfdf
jonrib <j.ribikauskas@sekasoft.com>
2020-06-11 21:42:04 +0300

    Small fix

--- src/main/java/com/jonrib/tasks/web/ResourceEntryController.java
+++ src/main/java/com/jonrib/tasks/web/ResourceEntryController.java
@@ -99,8 +99,9 @@ public class ResourceEntryController {
 	public ResponseEntity<String> postEntry(@RequestBody String entryJson, HttpServletRequest request){
 		try {
 			if (securityService.findLoggedInUsername(DataController.getJWTCookie(request.getCookies())).equals("anonymousUser"))
-				return new ResponseEntity<String>("You need to be logged in to create entries", HttpStatus.BAD_REQUEST);
-			ResourceEntry newEntry = mapper.readValue(entryJson, ResourceEntry.class);
+				return new ResponseEntity<String>("You need to be logged in to create entries", HttpStatus.FORBIDDEN);
+			JsonNode root = mapper.readTree(entryJson);
+			ResourceEntry newEntry = mapper.treeToValue(root.at("/entry"), ResourceEntry.class);
 			Set<User> author = new HashSet<User>();
 			author.add(userService.findByUsername(securityService.findLoggedInUsername(DataController.getJWTCookie(request.getCookies()))));
 			newEntry.setAuthor(author);
@@ -117,7 +118,23 @@ public class ResourceEntryController {
 			created.setUserName(securityService.findLoggedInUsername(DataController.getJWTCookie(request.getCookies())));
 			historyRepository.save(created);
 			newEntry.getHistories().add(created);
-			resourceEntryService.save(newEntry);
+			Set<User> readers = new HashSet<User>();
+			for (Iterator<JsonNode> i = root.at("/readers").elements(); i.hasNext();) {
+				String username = i.next().asText();
+				User usr = userService.findByUsername(username);
+				if (usr != null)
+					readers.add(usr);
+			}
+			Set<User> editors = new HashSet<User>();
+			for (Iterator<JsonNode> i = root.at("/editors").elements(); i.hasNext();) {
+				String username = i.next().asText();
+				User usr = userService.findByUsername(username);
+				if (usr != null)
+					editors.add(usr);
+			}
+			newEntry.setReaders(readers);
+			newEntry.setEditors(editors);
+			return new ResponseEntity<String>(mapper.writeValueAsString(resourceEntryService.save(newEntry)), HttpStatus.OK);
 		}catch (Exception e) {
 			return new ResponseEntity<String>(e.toString(), HttpStatus.BAD_REQUEST);
 		}
