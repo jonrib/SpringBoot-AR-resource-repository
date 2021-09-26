d5129ce7009157047db2476036abeeda5cffdc91
jonrib <jonas.ribikauskas@gmail.com>
2021-09-26 15:24:15 +0300

    Changes for HTTPS support and running prototype application

--- src/main/java/com/jonrib/app/web/ResourceFileController.java
+++ src/main/java/com/jonrib/app/web/ResourceFileController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -83,7 +84,7 @@ public class ResourceFileController {
 		}
 		Resource actualFile = storageService.loadAsResource(file.get().getFilePath());
 		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
-				"attachment; filename=\"" + actualFile.getFilename() + "\"").body(actualFile);
+				"attachment; filename=\"" + actualFile.getFilename() + "\"").contentType(MediaType.parseMediaType("application/octet-stream")).body(actualFile);
 	}
 	
 	@ExceptionHandler(BadResourceFileForEntryException.class)
