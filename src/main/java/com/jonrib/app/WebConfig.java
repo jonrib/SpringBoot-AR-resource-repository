d3c9ed8af2140a647d7456bf5d8f5f9f6f5fe5c1
jonrib <jonas.ribikauskas@gmail.com>
2021-11-09 19:42:16 +0200

    Changes for database

--- src/main/java/com/jonrib/app/WebConfig.java
+++ src/main/java/com/jonrib/app/WebConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("https://192.168.0.91:8100").exposedHeaders("Content-Disposition");
+        registry.addMapping("/**").allowedOrigins("*").exposedHeaders("Content-Disposition");
     }
 }
