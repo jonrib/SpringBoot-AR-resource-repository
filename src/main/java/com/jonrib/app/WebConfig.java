c26d5c4c30dbdeabbe19b58da24461694ebb4a64
jonrib <jonas.ribikauskas@gmail.com>
2021-12-19 20:56:56 +0200

    Fix for static not served

--- src/main/java/com/jonrib/app/WebConfig.java
+++ src/main/java/com/jonrib/app/WebConfig.java
@@ -3,6 +3,7 @@ package com.jonrib.app;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,21 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").exposedHeaders("Content-Disposition");
+    }
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/",
+            "classpath:/resources/",
+            "classpath:/static/",
+            "classpath:/public/",
+            "classpath:/custom/",
+            "file:/opt/myfiles/"
+    };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS)
+                .setCachePeriod(3000);
     }
 }
